824b79b6a328a7702eac8f3d4d2dc97211a0f775
Jake Hamilton <jakehamtexas@gmail.com>
2020-03-25 11:15:28 -0500

    Adds IdentityController for inspecting auth through the eyes of identity server

---
+++ client-credentials/src/Api/IdentityController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Api
+{
+    [Route("identity")]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var claimJson = User.Claims.Select(claim => new { claim.Type, claim.Value });
+            return new JsonResult(claimJson);
+        }
+    }
+}
