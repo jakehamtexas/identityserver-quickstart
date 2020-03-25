8f16e6ee0b9a453f10aed7e8ef74fa5ab7d42c7b
Jake Hamilton <jakehamtexas@gmail.com>
2020-03-25 14:25:28 -0500

    Finishes tutorial with some expert knob twiddling

--- client-credentials/src/Client/Program.cs
+++ client-credentials/src/Client/Program.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
 
 namespace Client
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+            var discoveryDocument = client.GetDiscoveryDocumentAsync("http://localhost:5005").GetAwaiter().GetResult();
+
+            if (HasError(discoveryDocument))
+            {
+                return;
+            }
+
+            var tokenResponse = client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
+            {
+                Address = discoveryDocument.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            }).GetAwaiter().GetResult();
+
+            if (HasError(tokenResponse))
+            {
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = client.GetAsync("http://localhost:5001/identity").GetAwaiter().GetResult();
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+
+        static bool HasError (ProtocolResponse response) 
+        {
+            if (response.IsError)
+            {
+                Console.WriteLine(response.Error);
+            }
+            return response.IsError;
         }
     }
 }
