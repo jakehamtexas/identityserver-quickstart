aa5aa6483aa9e8844aed78326dca1fe1da30317c
Jake Hamilton <jakehamtexas@gmail.com>
2020-03-25 11:05:53 -0500

    Adds simple configuration for client to auth with API

--- client-credentials/src/IdentityServer/Config.cs
+++ client-credentials/src/IdentityServer/Config.cs
@@ -7,21 +7,40 @@ using System.Collections.Generic;
 
 namespace IdentityServer
 {
-    public static class Config
-    {
-        public static IEnumerable<IdentityResource> Ids =>
-            new IdentityResource[]
-            { 
+  public static class Config
+  {
+    public static IEnumerable<IdentityResource> Ids =>
+        new IdentityResource[]
+        {
                 new IdentityResources.OpenId()
-            };
+        };
 
-        public static IEnumerable<ApiResource> Apis =>
-            new ApiResource[] 
-            { };
-        
-        public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
-        
-    }
+    public static IEnumerable<ApiResource> Apis =>
+        new ApiResource[]
+        {
+                new ApiResource("api1", "My API")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new Client
+            {
+                ClientId = "client",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "api1" }
+            }
+        };
+
+  }
 }
